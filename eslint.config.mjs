c71c767d03d9fce173ed44f175c305af1053a085
kyoko-nr <kyokokitagawa@me.com>
2025-01-18 13:45:49 +0900

    added eslint, prettier and three.js

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      semi: "error",
+      quotes: "error",
+    },
+  }
+);
